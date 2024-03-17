37d140c5cf8d98477876182bb2bd30cb643a306a
KRANTI@45 <krantiendolkar@gmail.com>
2024-03-17 22:09:27 +0530

    Solution

---
+++ Q14.c
@@ -0,0 +1,36 @@
+//14. Write a program to accept a number and check whether it is Prime no.
+
+
+#include <stdio.h>
+
+int main()
+
+{
+
+    int i, num, temp = 0; 
+    printf("Enter the number: ");
+    scanf("%d", &num);
+    for (i = 2; i <= num / 2; i++)
+    {
+         if (num % i == 0) 
+		 {
+            temp++;
+            break;
+        }
+
+    }
+    if (temp == 0 && num != 1)
+    {
+        printf("%d is a Prime number\n", num);
+    }
+
+    else
+
+    {
+        printf("%d is not a Prime number\n", num);
+
+    }
+
+    return 0;
+
+}
